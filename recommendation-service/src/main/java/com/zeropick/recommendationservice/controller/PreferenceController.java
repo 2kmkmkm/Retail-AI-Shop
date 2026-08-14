abefaaa234927de5a3e43cf1995f24f09b0a0b76
2kmkmkm <02dlrudals@gmail.com>
2026-08-14 18:13:28 +0900

    fix: PreferenceController의 prefix를 /recommendation-service/preferences로 변경

--- recommendation-service/src/main/java/com/zeropick/recommendationservice/controller/PreferenceController.java
+++ recommendation-service/src/main/java/com/zeropick/recommendationservice/controller/PreferenceController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/preferences")
+@RequestMapping("/recommendation-service/preferences")
 @RequiredArgsConstructor
 public class PreferenceController {
 
