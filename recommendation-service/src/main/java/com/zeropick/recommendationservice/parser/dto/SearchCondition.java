942ce46fbea679d28d69c7b57dc94ca4667ccd6f
2kmkmkm <02dlrudals@gmail.com>
2026-08-17 01:47:53 +0900

    feat: SearchCondition에 필드(allergenExclude, kcalMin, kcalMax) 추가

--- recommendation-service/src/main/java/com/zeropick/recommendationservice/parser/dto/SearchCondition.java
+++ recommendation-service/src/main/java/com/zeropick/recommendationservice/parser/dto/SearchCondition.java
@@ -14,7 +14,10 @@ import java.math.BigDecimal;
 public class SearchCondition {
     private String category;
     private String sweetenerExclude;
+    private String allergenExclude;
     private BigDecimal sugarMax;
+    private BigDecimal kcalMin;
+    private BigDecimal kcalMax;
     private Integer maxPrice;
     private String query;
 }
