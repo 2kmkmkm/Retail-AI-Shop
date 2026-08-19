5fdf8873684340b2a0afc0897d013fc7b8c531f4
2kmkmkm <02dlrudals@gmail.com>
2026-08-19 11:52:52 +0900

    chore: recommendation-service, product-service 환경 변수 통일 및 LLM 환경 변수 추가

--- product-service/src/main/java/com/zeropick/productservice/config/KafkaConsumerConfig.java
+++ product-service/src/main/java/com/zeropick/productservice/config/KafkaConsumerConfig.java
@@ -24,11 +24,14 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.consumer.group-id:product-service}")
+    private String groupId;
+
     @Bean
     public ConsumerFactory<String, String> stringConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "product-service");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
